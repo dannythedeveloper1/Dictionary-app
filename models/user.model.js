7930e4a8f428a202376fee4dda41e05f403e9e51
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2022-01-27 18:55:14 -0500

    mongoose added

---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const User = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        
+    }
+)
+
+
+//     < body >
+//     <main>
+//         <nav>
+//             <h1>hello</h1>
+//             <h1>world</h1>
+//         </nav>
+//     </ main> </main>
+// </ body >
+
+// nav {
+//     background - color: D#9c27b0;
+//     width: 100vw;
+//     height: 10vh;
+//     display: flex;
+//     justify - content: flex - end;
+// .box {
+//         width: 50vw;
+//         height: 10vh;
+//         display: flex;
+//         justify - content: space - between;
+//         align - items: center;
+//     }
+// h1 {
+//         color: "white";
+//     }
